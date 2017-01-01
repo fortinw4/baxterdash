0bb7a773ab427019dfa378ab08ddac2dd976e855
Will Fortin <fortinw4@gmail.com>
2016-12-31 17:05:14 -0700

    fix num g

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 
 $(function(){
 
-	var mask = '<span style="position:absolute; top:600; left:68.7%; background-color:black;">000-0000</span>';
+	var mask = '<span style="position:absolute; top:600; left:73.5%; background-color:black;">000-0000</span>';
 
 	$('.num').append(mask);
 	$('.num').hover(function(){
